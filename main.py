21b1357a54a17cfdef9e409ad4157794ce8ba48b
AlexanderGPo <alexanderg.povarov@gmail.com>
2023-12-16 12:24:26 +0300

    detecting face landmarks

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import numpy
+import numpy as np
 import cv2
 
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
+
+base_options = python.BaseOptions(model_asset_path='sources/face_landmarker_v2_with_blendshapes.task')
+options = vision.FaceLandmarkerOptions(base_options=base_options,
+                                       output_face_blendshapes=True,
+                                       output_facial_transformation_matrixes=True,
+                                       num_faces=1)
+detector = vision.FaceLandmarker.create_from_options(options)
